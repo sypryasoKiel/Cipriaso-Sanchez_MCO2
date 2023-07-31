1981cfa9e3f523b9d966935fd6bf0d30337ca284
IceBear3 <jeck_sanchez@dlsu.edu.ph>
2023-07-31 21:05:07 +0800

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -9,14 +9,26 @@ public class Item {
         this.CALORIES = calories;
     }
 
+    /**
+     * gets the price of the item
+     * @return price the price
+     */
     public double getPrice(){
         return this.PRICE;
     }
 
+    /**
+     * gets the name of the item
+     * @return name the name
+     */
     public String getName(){
         return this.NAME;
     }
 
+    /**
+     * gets the amount of calories of the item
+     * @return
+     */
     public double getCalories(){
         return this.CALORIES;
     }
